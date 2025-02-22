f0fbeee7d4a826b88b2d6a024fc47b77dda74fa4
onlyidev <liudas.kasper@gmail.com>
2025-02-22 14:23:03 +0200

    Add MCA data

--- tests/MCA.py
+++ tests/MCA.py
@@ -5,31 +5,30 @@ import prince
 import matplotlib.pyplot as plt
 
 # Load the .npy file
-data = np.load('../data/trial_ben.npy')
+data_benign = np.load('../data/trial_ben.npy')
+df_benign = pd.DataFrame(data_benign)
 
-# Convert the numpy array to a DataFrame
-df = pd.DataFrame(data)
+data_malicious = np.load('../data/trial_mal.npy')
+df_malicious = pd.DataFrame(data_malicious)
 
-# Display the DataFrame
-df.head()
-
-mca = prince.MCA()
-mca = mca.fit(df)
-benign = mca.transform(df)
-
-# %%
-# Load the .npy file
-data = np.load('./data/trial_mal.npy')
-
-# Convert the numpy array to a DataFrame
-df = pd.DataFrame(data)
+df = pd.concat([df_benign, df_malicious], ignore_index=True)
 
 # Display the DataFrame
 df.head()
 
 mca = prince.MCA()
 mca = mca.fit(df)
-mal = mca.transform(df)
+transformed_data = mca.transform(df)
+
+# Separate the transformed data back into benign and malicious
+benign = transformed_data.iloc[:len(df_benign)]
+mal = transformed_data.iloc[len(df_benign):]
+
+# Create labels for each class
+labels = ['benign'] * len(df_benign) + ['malicious'] * len(df_malicious)
+
+# Add the labels as a new column to the transformed data
+transformed_data['class'] = labels
 
 # %%
 plt.scatter(benign.iloc[:, 0], benign.iloc[:, 1], label='Benign')
@@ -38,6 +37,4 @@ plt.legend()
 plt.show()
 
 # %%
-benign.to_csv()
-
-
+transformed_data.to_csv('../data/MCA.csv', index=False)
